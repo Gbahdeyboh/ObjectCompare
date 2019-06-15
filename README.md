885c6959afc059ca4c1ff8bc51ca43c62e25f637
Gbahdeyboh <gbahdeybohbello@gmail.com>
2019-06-15 04:27:12 +0000

    Updated the README.md file

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# ObjectCompare
+# JavaScript Object Comparer
 
-A simple boilerplate code that does deep comparism between two objects and hence checks for equality between them.
+When i was learning JavaScript some years back, it took me sometime to understand what Javascript Objects really are. I remember asking myself questions like why comparism operators doesn't work with Objects.
+
+I recently started reading a book by [javascript tutor](https://twitter.com/js_tut) titled [JavaScript grammar](http://javascriptgrammar.com/dark/). Towards the end of the book, he talked about comparing objects and gave a nice approach to how it could be acheieved. This reminded me of my question way back then, so i decided to write a script that does deep comparism on Objects.
+
+This little project could also serve as a good learning resource for anyone who is inquisitive enough to want to know how to really compare any kind of Objects(simple or complex). The code has been properly commented and formatted for readability and ease of understanding.
+
+## Build Setup
+
+```javascript
+#install dependencies
+npm install --save
+
+#run the script
+npm run start
+
+#to run test's
+npm run test
+```
+
+Go through the test file to see examples of complex objects it can compare.
+
+### Thank you!
--- index.js
+++ index.js
@@ -1,108 +1,108 @@
 /**
-* @author - Bello Gbadebo 
-* @licence - https://github.com/Gbahdeyboh/ObjectCompare/blob/master/LICENSE
-* @description - This module does a deep comparism between two Objects and return true if they match 
-* It supports the following types of Objects - Objects, Strings, Numbers, Functions, Booleans, Arrays, Sets and Dates 
-*/
+ * @author - Bello Gbadebo 
+ * @description - This module does a deep comparism between two Objects and return true if they match 
+ * licence - https://github.com/Gbahdeyboh/ObjectCompare/blob/master/LICENSE
+ * It supports the following types of Objects - Objects, Strings, Numbers, Functions, Booleans, Arrays, Sets and Dates 
+ */
 
-function reject(msg){
+function reject(msg) {
 	/**
-	* @param {String} [msg] - The error message being thrown
-	* @throw throws - the `msg` Error
+	 * @param {String} [msg] - The error message being thrown
+	 * @throws - throws - the `msg` Error
 	 */
 	throw new Error(msg);
 }
-function compareObjects(obj1, obj2){
+
+function compareObjects(obj1, obj2) {
 	/**
-	* This function does a deep comparism between two Object passed as arguments to it
-	* @param {Object} [obj1 | obj2] - Objects being compared
+	 * This function does a deep comparism between two Object passed as arguments to it
+	 * @param {Object} [obj1 | obj2] - Objects being compared
 	 */
 	'use strict';
 	//first confirm if both parameters passed to the arguments are objects
-	if(typeof obj1 !== 'object' || typeof obj2 !== 'object'){
+	if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
 		reject("Parameters passed into the arguments must be Objects")
 	};
 	//Then check if the Objects have different constructors
-	if(obj1.constructor !== obj2.constructor){
+	if (obj1.constructor !== obj2.constructor) {
 		reject("Objects have different constructors!");
 	}
 	//The keys of each Objects which is sorted to aid uniformity between the key Array of both objects
 	let objKeys1 = Object.keys(obj1).sort();
 	let objKeys2 = Object.keys(obj2).sort();
 	//If the keys are of different lengths
-	if(objKeys1.length !== objKeys2.length){
+	if (objKeys1.length !== objKeys2.length) {
 		reject("Ooops!, the objects passed in are structured differently")
 	}
 	//Loops through every value of the Object
-	for(let i in objKeys1){
+	for (let i in objKeys1) {
 		//check if both object have the same keys arranged in the same order
-		if(objKeys1[i] !== objKeys2[i]){
+		if (objKeys1[i] !== objKeys2[i]) {
 			reject("Ooops!, the objects passed in are structured differently. Their keys mismatched!")
-		}
-		else{
-			if(typeof obj1[objKeys1] !== typeof obj2[objKeys1]){
+		} else {
+			if (typeof obj1[objKeys1] !== typeof obj2[objKeys1]) {
 				//The value of each Object must always be the same 
 				reject("Object values are of different types!");
-			}
-			else{
+			} else {
 				//handle each type of supported values differently
 				const value1 = obj1[objKeys1[i]];
 				const value2 = obj2[objKeys1[i]];
-				function valueHandler(val1, val2){
-					switch(typeof val1){
-						case 'number' : 
-							if(val1 !== val2){
+
+				function valueHandler(val1, val2) {
+					switch (typeof val1) {
+						case 'number':
+							if (val1 !== val2) {
 								reject("An Object value of type `number` mismatched");
 							}
 							break;
-						case 'boolean' : 
-							if(val1 !== val2){
+						case 'boolean':
+							if (val1 !== val2) {
 								reject("An Object value of type `boolean` mismatched");
 							}
 							break;
-						case 'string' : 
-							if(val1 !== val2){
+						case 'string':
+							if (val1 !== val2) {
 								reject("An Object value of type `string` mismatched");
 							}
 							break;
-						case 'function' :
-							if(val1.call(val1) !== val2.call(val2)){
+						case 'function':
+							if (val1.call(val1) !== val2.call(val2)) {
 								reject("An Object value of type `function` mismatched")
 							}
 							break;
-						case 'object' :
+						case 'object':
 							/*
-							* An Object could be anything, literally everything in javascript is an Object.
-							* Different kinds of objects are ompared differently. Date Objects can't be compared the
-							* same way Array or Sets or Regular Expressions are compared. Each type of Object are compared
-							* to each other differently as shown below
-							*/
+							 * An Object could be anything, literally everything in javascript is an Object.
+							 * Different kinds of objects are ompared differently. Date Objects can't be compared the
+							 * same way Array or Sets or Regular Expressions are compared. Each type of Object are compared
+							 * to each other differently as shown below
+							 */
 							// Every Array is an Object, so we first have to confirm if the Object is an array before proceeding
-							if(Array.isArray(val1) && Array.isArray(val2)){
-								for(let j in val1){
+							if (Array.isArray(val1) && Array.isArray(val2)) {
+								for (let j in val1) {
 									//loop through the array and handle each data type in the array recursively
 									valueHandler(val1[j], val2[j]);
 								}
 							}
 							//Check if both Objects are instances of a Regular Expresion
-							if(val1 instanceof RegExp || val2 instanceof RegExp){
+							if (val1 instanceof RegExp || val2 instanceof RegExp) {
 								String(val1) !== String(val2) ? reject("The regular Expressions are different") : true;
 							}
 							//Check if both Objects are instances of a Date Object
-							if(val1 instanceof Date || val2 instanceof Date){
+							if (val1 instanceof Date || val2 instanceof Date) {
 								val1.valueOf() === val2.valueOf() ? true : reject("The Date Objects do not match")
 							}
 							//Check if both functions are instanes of a Set Object
-							if(val1 instanceof Set || val2 instanceof Set){
+							if (val1 instanceof Set || val2 instanceof Set) {
 								val1.size !== val2.size ? reject("The Set's Objects are of different sizes") : true;
-								for (let a of val1){
+								for (let a of val1) {
 									if (!val2.has(a)) reject("The Sets values are not equal");
 								}
 							}
 							//If it's a regular object, call the parent function (compareObjects) recursively
 							compareObjects(val1, val2);
 							break;
-						default: 
+						default:
 							reject("An Object value type that isn't supported was passed in");
 
 					}
@@ -112,11 +112,7 @@ function compareObjects(obj1, obj2){
 		}
 	}
 	return true;
-	
+
 }
 
 module.exports = compareObjects;
-
-
-
-
